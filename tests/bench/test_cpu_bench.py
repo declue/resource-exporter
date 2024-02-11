e223b3d5a35364e932822d585e38f637227f8c9b
declue <bkperio@gmail.com>
2024-02-11 22:45:28 +0900

    test code 추가

---
+++ tests/bench/test_cpu_bench.py
@@ -0,0 +1,9 @@
+from resource_exporter.bench.cpu_bench import CPUBench
+
+
+def test_single():
+    print(CPUBench().test_single_thread())
+
+
+def test_full():
+    print(CPUBench().test_full_threads())
